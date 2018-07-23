6864399995a632d9db59538785abdae64d59de68
Abigail Slovick <abby.slovick@gmail.com>
2018-07-23 21:04:55 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -1,5 +1,5 @@
 puts "How To Do The Macarena:"
-# puts ""
+puts ""
 puts "Step 1: Right arm out in front of you, palm facing down."
 puts "Step 2: Left arm out in front of you, palm facing down."
 puts "Step 3: Turn your right palm up."
